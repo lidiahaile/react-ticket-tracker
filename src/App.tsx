29db919983105d890185e8d26e419733bb1a67cd
Lidia Haile <lidiahaile@gmail.com>
2024-04-18 07:40:48 +0100

    tickets are displayed and stryled

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import "./App.scss";
+import Employee from "./components/Employee/Employee";
+import team from "./Data/TeamData"
 
 const App =( ) =>{
 
 
 
 
+
   return (
-    <div>
-     
-      
+    
+    <div className="ticket-tracker__container">
+      <h1 className="ticket-tracker__title">Ticket Tracker</h1>
+      {team.map(({ name, role }) => (
+        <Employee key={name} name={name} role={role} />
+      ))}
     </div>
   )
-}
+      };
 
-export default App
-
+export default App;
+  
---
+++ src/components/Employee/Employee.tsx
@@ -0,0 +1,43 @@
+import  { useState } from 'react'
+import "./Employee.scss"
+
+
+type EmployeeProps = {
+   name:string;
+   role:string;
+    
+}
+
+const Employee =({name,role}:EmployeeProps) =>{
+const [ticketCount,setTicketCount]= useState<number>(0);
+
+//increment the number
+const incrementTicket =() =>{
+    setTicketCount(ticketCount+1)
+};
+
+
+//decrease the number
+const decrementTicket = () =>{
+    if(ticketCount > 0){
+    setTicketCount(ticketCount-1)
+    }
+}
+
+  return (
+    <div >
+    <div className="ticket-tracker">
+      <p>{name}</p>
+      <p>{role}</p>
+      <div className="ticket-tracker__counter">
+        <p >Counter</p>
+        <p >{ticketCount}</p>
+        <button className="ticket-tracker__button"onClick={incrementTicket}> +</button>
+        <button className="ticket-tracker__button" onClick={decrementTicket}>-</button>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Employee
